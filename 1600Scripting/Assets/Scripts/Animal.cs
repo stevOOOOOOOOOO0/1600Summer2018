f01abe14cdf77aa2584df188955505280280bf33
Unknown <stevOOOOOOOOOO0@yahoo.com>
2018-05-16 11:19:06 -0600

    Rotation & Translation
    get it boi

---
+++ 1600Scripting/Assets/Scripts/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+    // create variable i
+    int counter = 150;
+
+    public float Speed = 1;
+    public int PowerLevel = 0;
+    public string PlayerName = "Unknown";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (counter < 300)
+        {
+            transform.Rotate(0, Speed, 0);
+            counter++;
+        }
+        else
+        {
+            transform.Rotate(0, Speed * -1, 0);
+            counter++;
+        }
+        if (counter == 600)
+            counter = counter % 20;
+        transform.Translate(Speed, 0, 0);
+
+    }
+}
---
+++ 1600Scripting/Assets/Scripts/StartAndUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAndUpdate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print("Hello World");
+	}
+}
