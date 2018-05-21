40db41dddcd7fb205175aa330081feac448bddba
Unknown <stevOOOOOOOOOO0@yahoo.com>
2018-05-21 10:15:29 -0600

    Movement Script
    moving the cube or object with the arrow keys
    
    also created a very simple scene for it

---
+++ 1600Scripting/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        print(Input.GetAxis("Horizontal"));
+
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
