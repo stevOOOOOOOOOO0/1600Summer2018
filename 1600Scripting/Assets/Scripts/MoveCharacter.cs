286f349d78c601bb9878eb926862847d75ba0342
Unknown <stevOOOOOOOOOO0@yahoo.com>
2018-06-12 11:16:50 -0600

    Movement using Character Controller

---
+++ 1600Scripting/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+    private CharacterController controller;
+	
+	private Vector3 newPosition;
+	public float Speed = 10.0f;
+	private float gravity = -20.0f;
+    
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		newPosition.z = Speed * Time.deltaTime;
+		newPosition.y = gravity * Time.deltaTime;
+		newPosition.x = Speed * Time.deltaTime * Input.GetAxis("Horizontal");
+		controller.Move(newPosition);
+	}
+}
+ 
