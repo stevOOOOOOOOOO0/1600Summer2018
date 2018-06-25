ac7031710310d1bd6c47ad247ccc6744a0c2d3fa
Unknown <stevOOOOOOOOOO0@yahoo.com>
2018-06-25 11:27:30 -0600

    Created a Gun
    recreates a game object: Ammo

---
+++ 1600Scripting/Assets/Scripts/6252018/Ammo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour {
+
+   private Rigidbody projectile;
+   public FloatData ProjectileSpeed;
+   public FloatData ProjectileUpgrade;
+
+	// Use this for initialization
+	void Start () {
+      projectile = GetComponent<Rigidbody>();
+      projectile.AddForce(0, 0, ProjectileSpeed.GetValue());
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+      ProjectileSpeed = ProjectileUpgrade;
+      projectile.AddForce(0, 0, ProjectileSpeed.GetValue());
+   }
+}
---
+++ 1600Scripting/Assets/Scripts/6252018/FireAmmo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAmmo : MonoBehaviour {
+
+   public GameObject Ammo;
+   
+	// Update is called once per frame
+	void Update () {
+      if (Input.GetKeyDown(KeyCode.Space))
+         Instantiate(Ammo);
+	}
+}
