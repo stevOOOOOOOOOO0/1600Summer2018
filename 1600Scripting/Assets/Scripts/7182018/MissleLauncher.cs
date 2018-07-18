79c125da67dcc27ebeecde68966b9cca47b03521
Unknown <stevOOOOOOOOOO0@yahoo.com>
2018-07-18 12:27:41 -0600

    created a missle with personal AI
    shoots out of launcher at 10,10,10 speed and then moves to players position.

--- 1600Scripting/Assets/Scripts/7182018/MissleLauncher.cs
+++ 1600Scripting/Assets/Scripts/7182018/MissleLauncher.cs
@@ -15,39 +15,39 @@ public class MissleLauncher : MonoBehaviour
 	void Start ()
 	{
 		Missle = GetComponent<CharacterController>();
-		movement = new Vector3(15, 15, 15);
+		movement = new Vector3(10, 10, -10);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		getMovement();
 		Missle.Move(movement * Time.deltaTime);
+		getMovement();
 	}
 
 	private void getMovement()
 	{
-		if (Missle.transform.position.x < Destination.position.x)
+		if (Missle.transform.position.x < Destination.position.x && movement.x < 10)
 		{
 			movement.x += speed;
 		}
-		else if (Missle.transform.position.x > Destination.position.x)
+		else if (Missle.transform.position.x > Destination.position.x && movement.x > -10)
 		{
 			movement.x -= speed;
 		}
-		if (Missle.transform.position.y < Destination.position.y)
+		if (Missle.transform.position.y < Destination.position.y && movement.y < 10)
 		{
 			movement.y += speed;
 		}
-		else if (Missle.transform.position.y > Destination.position.y)
+		else if (Missle.transform.position.y > Destination.position.y && movement.y > -10)
 		{
 			movement.y -= speed;
 		}
-		if (Missle.transform.position.z < Destination.position.z)
+		if (Missle.transform.position.z < Destination.position.z && movement.z < 10)
 		{
 			movement.z += speed;
 		}
-		else if (Missle.transform.position.z > Destination.position.z)
+		else if (Missle.transform.position.z > Destination.position.z && movement.z > -10)
 		{
 			movement.z -= speed;
 		}
