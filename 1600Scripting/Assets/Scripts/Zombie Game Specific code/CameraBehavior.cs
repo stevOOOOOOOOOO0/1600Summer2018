a254632581b29b41c355f119e1824e65c7fca796
Unknown <stevOOOOOOOOOO0@yahoo.com>
2018-08-03 15:18:53 -0600

    player mouse behavior and camera behavior

---
+++ 1600Scripting/Assets/Scripts/Zombie Game Specific code/CameraBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+	public Transform PlayerPosition;
+	private Transform cameraPosition;
+	private Vector3 tempPosition;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		cameraPosition = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		tempPosition = PlayerPosition.position;
+		tempPosition.y = 40;
+		cameraPosition.position = tempPosition;
+	}
+}
---
+++ 1600Scripting/Assets/Scripts/Zombie Game Specific code/PlayerDirectionalBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDirectionalBehavior : MonoBehaviour
+{
+
+	public Vector3 mousePosition;
+	public Transform PlayerPosition;
+
+	private void Start()
+	{
+		PlayerPosition = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		mousePosition.z = mousePosition.y;
+		mousePosition.y = 1.08f;
+		mousePosition.x += PlayerPosition.position.x - .5f;
+		mousePosition.z += PlayerPosition.position.z - .5f;
+		PlayerPosition.LookAt(mousePosition);
+	}
+}
